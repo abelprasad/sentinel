5b25b3754cfc93ad26763da6da97d76b978b0855
abelprasad <abelprasad4@gmail.com>
2026-06-01 19:51:14 -0400

    feat: package structure + AircraftEntity JPA model

--- src/main/java/com/abel/sentinel/controller/HealthController.java
+++ src/main/java/com/abel/sentinel/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.abel.sentinel;
+package com.abel.sentinel.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/abel/sentinel/model/AircraftEntity.java
@@ -0,0 +1,18 @@
+package com.abel.sentinel.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "entities")
+public class AircraftEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+    private String callsign;
+    private String metadata;
+}
